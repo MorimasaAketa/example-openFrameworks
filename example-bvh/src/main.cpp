3cd0f6c86102d8fe44f4b31e65f155659907733a
Morimasa Aketa <morimasa.aketa@gmail.com>
2018-12-11 14:08:34 +0900

    update for of 0.10

--- example-bvh/src/main.cpp
+++ example-bvh/src/main.cpp
@@ -1,11 +1,9 @@
 #include "ofMain.h"
 #include "ofApp.h"
-#include "ofAppGlutWindow.h"
+//#include "ofAppGlutWindow.h"
 
 //========================================================================
 int main( ){
-
-    ofAppGlutWindow window;
 	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
